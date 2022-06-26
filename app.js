483203a69bd06c6fd703d2f7e180c9fd66b4c4c8
wahyurahmana <tn.wahyurahmana@gmail.com>
2022-06-26 21:03:35 +0800

    deploy heroku and use atlas

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const {connectToServer, getDb} = require('./db/conn')
 const allRouter = require('./routes')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const cors = require('cors')
 
 app.use(express.json())
