80240c895e21404c8bddeeec18b28e3c06e91192
wahyurahmana <tn.wahyurahmana@gmail.com>
2022-06-26 23:09:51 +0800

    fixing bug deploy to aws

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const {connectToServer} = require('./db/conn')
 const allRouter = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
+const host = process.env.NODE_ENV !== 'production' ? '127.0.0.1' : '0.0.0.0'
 const cors = require('cors')
 
 app.use(express.json())
@@ -13,7 +14,7 @@ app.use(cors())
 connectToServer()
 .then((result) => {
   console.log(result)
-  app.listen(port, () => {
+  app.listen(port, host, () => {
     console.log(`app listening on port ${port}`)
   })
 }).catch((err) => {
